fe958887203dd1b01040d869202aa92799e6c5b0
Jeremias Belucci <jerebelucci@gmail.com>
2022-11-09 22:00:47 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+
+ 
+ function delivery () {
+      let platos = parseInt(prompt ("¿Cuantos platos de ramen desea?"))
+      if (isNaN(platos)){
+          alert("Por favor elija un numero valido")
+          delivery()
+      } else{
+          alert("Usted ordenó " + platos + " platos de ramen");
+          let tipoDeRamen = (prompt ("¿Prefiere su ramen a base de fideos o de arroz?").toLowerCase())
+          switch(tipoDeRamen){
+              case "fideos":
+                  let direccion = prompt ("¿A que direccion enviamos el pedido?")
+                  alert ("Su orden de " + platos + " de ramen de " + tipoDeRamen + " fue recibida con exito, sera preparada en breves y enviada a " + direccion)
+                  break;
+              case "arroz":
+                  let direccionn = prompt ("¿A que direccion enviamos el pedido?")
+                  alert ("Su orden de " + platos + " de ramen de " + tipoDeRamen + " fue recibida con exito y sera preparada y enviada a " + direccionn)
+                  break;
+          }
+      }
+
+  }
+
+  delivery() 
